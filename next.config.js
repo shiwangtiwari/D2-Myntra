45b52596ade33e083a44d0cb8653c0a874bbacce
shiwangtiwari <shiwangtiwari5@gmail..com>
2026-08-19 14:35:28 +0530

    Fix Next.js 16 Turbopack config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Allow importing JSON files as modules
-  webpack: (config) => {
-    config.resolve.extensionAlias = {
-      ...config.resolve.extensionAlias,
-    };
-    return config;
-  },
+  turbopack: {},
 };
 
 module.exports = nextConfig;
